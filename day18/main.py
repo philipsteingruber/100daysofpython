d33256c3854307b4cf2aaab1e380f7e8603c1424
= <=>
2024-06-28 15:10:47 +0200

    Day 18 done.

--- day18/main.py
+++ day18/main.py
@@ -1,8 +1,31 @@
 import colorgram
+from turtle import Turtle, Screen
+import random
 
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
 for color in colors:
     rgb_colors.append(color.rgb)
 
-print(rgb_colors)
+turtle = Turtle()
+screen = Screen()
+screen.colormode(255)
+
+turtle.pensize(25)
+turtle.speed(0)
+turtle.penup()
+
+step = 50
+
+turtle.sety(step)
+turtle.setx(step)
+for i in range(10):
+    turtle.sety(step * (i+1))
+    for j in range(10):
+        turtle.dot(25, random.choice(rgb_colors))
+        turtle.setx(step * (j+1))
+
+
+
+
+screen.exitonclick()
