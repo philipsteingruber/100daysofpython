c620fa75a61635df28591c83f1f86a10a16d4be3
= <=>
2024-07-04 10:19:18 +0200

    Day 33 done.

--- day33/main.py
+++ day33/main.py
@@ -5,38 +5,31 @@ import requests
 
 MY_LAT = 57.722225  # Your latitude
 MY_LONG = 11.935108  # Your longitude
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-
-def refresh_iss():
-    iss_latitude = float(data["iss_position"]["latitude"])
-    iss_longitude = float(data["iss_position"]["longitude"])
-    return iss_latitude, iss_longitude
-
-
-# Your position is within +5 or -5 degrees of the ISS position.
-
-
-parameters = {
+REQUEST_PARAMETERS = {
     "lat": MY_LAT,
     "lng": MY_LONG,
     "formatted": 0,
 }
 
 
+def refresh_iss():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    return iss_latitude, iss_longitude
+
+
 def refresh_sunrise_sunset():
-    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response = requests.get(
+        "https://api.sunrise-sunset.org/json", params=REQUEST_PARAMETERS
+    )
     response.raise_for_status()
     data = response.json()
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
     return sunrise, sunset
-
-
-time_now = datetime.now()
 
 
 def iss_close(iss_latitude, iss_longitude):
@@ -49,9 +42,11 @@ def sun_is_down(sunrise, sunset):
 
 
 while True:
-    iss_latitude, iss_longitude = refresh_iss()
-    sunrise, sunset = refresh_sunrise_sunset()
-    if sun_is_down(sunrise, sunset) and iss_close(iss_latitude, iss_longitude):
+    current_iss_latitude, current_iss_longitude = refresh_iss()
+    lastest_sunrise, latest_sunset = refresh_sunrise_sunset()
+    if sun_is_down(lastest_sunrise, latest_sunset) and iss_close(
+        current_iss_latitude, current_iss_longitude
+    ):
         print("go")
     else:
         print("nope")
